d98c748c869874216a75577b08cd7f67810c1c5f
simon-saffayeh <93934522+simon-saffayeh@users.noreply.github.com>
2024-04-04 09:50:35 -0400

    sdfsdf
    sdfsdf

--- sql/src/App.vue
+++ sql/src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import { supabase } from './lib/supabaseClient'
+
 </script>
 
 <template>
---
+++ sql/src/lib/supabaseClient.js
@@ -0,0 +1,2 @@
+import { createClient } from "@supabase/supabase-js";
+export const supabase = createClient('https://bupfgvujmfpqumcodids.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1cGZndnVqbWZwcXVtY29kaWRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIxNTM0NzUsImV4cCI6MjAyNzcyOTQ3NX0.-0wUvgPIPXAq_pzMPwpZffx28nosYJeef1mr4N4PfJo')
