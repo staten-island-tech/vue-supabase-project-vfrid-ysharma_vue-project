6bf7dd4213daa173904a01127f7d53aeed386821
Yan Sharma <yawning008@gmail.com>
2024-04-05 10:15:27 -0400

    awasd

--- sql/src/App.vue
+++ sql/src/App.vue
@@ -2,21 +2,41 @@
   import { ref, onMounted } from 'vue'
   import { supabase } from './lib/supabaseClient'
 
-  const Users = ref([])
+  // const Users = ref([])
+  // async function getUsers() {
+  //   let { data } = await supabase.from('Users').select()
+  //   Users.value = data
+  // }
+  // onMounted(() => {
+  //   getUsers()
+  // })
+  // console.log(Users.value)
 
-  async function getUsers() {
-    const { data } = await supabase.from('Users').select()
-    Users.value = data
+   const email = ref('')
+   const password = ref('')
+   const f_name =ref('')
+   const grade =ref()
+  async function signup() {
+    let { data, error } = await supabase.auth.signUp({
+    email: email.value,
+    password: password.value,
+    options: {
+      data: {
+        first_name: f_name.value,
+        grade: grade.value
+      }
+    }
   }
-
-  onMounted(() => {
-    getUsers()
   })
-  console.log(Users)
+  console.log(data)
+  console.log(email.value, password)
+  }
   </script>
 
   <template>
-    <ul>
-      <li v-for="user in Users" >{{ user.username }}<img :src="user.profile_pic" ></li>
-    </ul>
+    <div>
+    <input v-model="email" type="text" placeholder="email" />
+    <input v-model="password" type="text" placeholder="password" />
+    <button @click="signup">Sign Up</button>
+  </div>
   </template>
