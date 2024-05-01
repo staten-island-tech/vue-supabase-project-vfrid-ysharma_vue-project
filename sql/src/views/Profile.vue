dee9ca2d3d252f1a27781b492510efe922ebdb8b
Vfrid <113192881+Vfrid@users.noreply.github.com>
2024-05-01 10:15:03 -0400

    noeditingrn

--- sql/src/views/Profile.vue
+++ sql/src/views/Profile.vue
@@ -14,45 +14,46 @@
       .eq('username',username )
     info.value = data
   }
-  async function updateProfile(grade,email,f_name,l_name){
-    const {new_data} = await supabase.from('Users')
-      .upsert({username:username, grade:grade, email:email, f_name:f_name, l_name:l_name})
-      .select()
-      .eq('username',username)
-    console.log(grade,email,f_name,l_name)
-    info.value = new_data
-    }
-  function openForm(){
-    const inform = document.querySelector(".info")
-    inform.innerHTML=(
-      `    <form action="" id="form" class="form_container">
-      <label for="grade" class="form_label">Grade: </label>
-      <input type="text" name="grade" id="grade" class="input_field"/>
-      <label for="email" class="form_label">Email: </label>
-      <input type="text" name="email" id="email" class="input_field"/>
-      <label for="f_name" class="form_label">First Name: </label>
-      <input type="text" name="f_name" id="f_name" class="input_field"/>
-      <label for="l_name" class="form_label">Last Name: </label>
-      <input type="text" name="l_name" id="l_name" class="input_field"/>
-      <input type="submit" value="Update Information" class="btn"/>
-    </form>`
-    )
-    const grade = document.querySelector("#grade").value
-    const email = document.querySelector("#email").value
-    const f_name = document.querySelector("#f_name").value
-    const l_name = document.querySelector("#l_name").value
-    console.log('changed',grade,email,f_name,l_name)
-    const form = document.querySelector("#form")
-    form.addEventListener("submit", function(e){
-    e.preventDefault();
-    updateProfile(grade,email,f_name,l_name)
-  })
-  }
+  // async function updateProfile(grade,email,f_name,l_name){
+  //   const {new_data} = await supabase.from('Users')
+  //     .upsert({username:username, grade:grade, email:email, f_name:f_name, l_name:l_name})
+  //     .select()
+  //     .eq('username',username)
+  //   console.log(grade,email,f_name,l_name)
+  //   info.value = new_data
+  //   }
+  // function openForm(){
+  //   const inform = document.querySelector(".info")
+  //   inform.innerHTML=(
+  //     `    <form action="" id="form" class="form_container">
+  //     <label for="grade" class="form_label">Grade: </label>
+  //     <input type="text" name="grade" id="grade" class="input_field"/>
+  //     <label for="email" class="form_label">Email: </label>
+  //     <input type="text" name="email" id="email" class="input_field"/>
+  //     <label for="f_name" class="form_label">First Name: </label>
+  //     <input type="text" name="f_name" id="f_name" class="input_field"/>
+  //     <label for="l_name" class="form_label">Last Name: </label>
+  //     <input type="text" name="l_name" id="l_name" class="input_field"/>
+  //     <input type="submit" value="Update Information" class="btn"/>
+  //   </form>`
+  //   )
+  //   const grade = document.querySelector("#grade").value
+  //   const email = document.querySelector("#email").value
+  //   const f_name = document.querySelector("#f_name").value
+  //   const l_name = document.querySelector("#l_name").value
+  //   console.log('changed',grade,email,f_name,l_name)
+  //   const form = document.querySelector("#form")
+  //   form.addEventListener("submit", function(e){
+  //   e.preventDefault();
+  //   updateProfile(grade,email,f_name,l_name)
+  // })
+  // }
   onMounted(() => {
     getProfile()
   })
   
   console.log(info)
+
 </script>
 
 <template>
@@ -63,7 +64,7 @@
         <h1 class="full_name_title"> {{data.f_name + " "+ data.l_name}} </h1>
       </div>
       <h2 class="username">{{ data.username }}</h2>
-      <button class="edit_pic_button">Change Profile Picture</button>
+      <!-- <button class="edit_pic_button">Change Profile Picture</button> -->
     </div>
     <div class="info">
       <h2>{{"Grade: "+ data.grade }}</h2>
@@ -71,7 +72,7 @@
       <h2>{{ "Date Account Was Created: "+data.created_at }}</h2>
       <h2>{{"First Name: "+ data.f_name }}</h2>
       <h2>{{ "Last Name: "+data.l_name }}</h2>
-      <button class="edit_button" @click="openForm()">Edit</button>
+      <!-- <button class="edit_button" @click="openForm()">Edit</button> -->
     </div>
   </div>
 
